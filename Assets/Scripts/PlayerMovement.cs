55f5729a3d2c70fa378cb05baae91d6d1538a64f
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-04 09:48:49 -0700

    Begginings of movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,64 +1,21 @@
-// This first example shows how to move using Input System Package (New)
-
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Example : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private float playerSpeed = 5.0f;
-    private float jumpHeight = 1.5f;
-    private float gravityValue = -9.81f;
-
-    public CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-
-    [Header("Input Actions")]
-    public InputActionReference moveAction;
-    public InputActionReference jumpAction;
-
-    private void OnEnable()
+    double Speed = 20;
+    double Jump = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        moveAction.action.Enable();
-        jumpAction.action.Enable();
+        
     }
 
-    private void OnDisable()
-    {
-        moveAction.action.Disable();
-        jumpAction.action.Disable();
-    }
-
+    // this is a update that happends every couple of frames rather than everyframe
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-
-        if (groundedPlayer)
+        if (Input.GetButtonDown("w"))
         {
-            // Slight downward velocity to keep grounded stable
-            if (playerVelocity.y < -2f)
-                playerVelocity.y = -2f;
+            Debug.Log("forward");
         }
-
-        // Read input
-        Vector2 input = moveAction.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
-        move = Vector3.ClampMagnitude(move, 1f);
-
-        if (move != Vector3.zero)
-            transform.forward = move;
-
-        // Jump using WasPressedThisFrame()
-        if (groundedPlayer && jumpAction.action.WasPressedThisFrame())
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
-        }
-
-        // Apply gravity
-        playerVelocity.y += gravityValue * Time.deltaTime;
-
-        // Move
-        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
-        controller.Move(finalMove * Time.deltaTime);
     }
 }

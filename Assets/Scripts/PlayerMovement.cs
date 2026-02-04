7c3383ba0bd385f13621f5bc7bc2aa7af9879a30
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-04 09:35:43 -0700

    movement system?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,64 @@
-using UnityEngine;
+// This first example shows how to move using Input System Package (New)
 
-public class PlayerMovement : MonoBehaviour
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Example : MonoBehaviour
 {
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float playerSpeed = 5.0f;
+    private float jumpHeight = 1.5f;
+    private float gravityValue = -9.81f;
+
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    [Header("Input Actions")]
+    public InputActionReference moveAction;
+    public InputActionReference jumpAction;
+
+    private void OnEnable()
     {
-        
+        moveAction.action.Enable();
+        jumpAction.action.Enable();
     }
 
-    // this is a update that happends every couple of frames rather than everyframe
-    void LateUpdate()
+    private void OnDisable()
     {
-        
+        moveAction.action.Disable();
+        jumpAction.action.Disable();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer)
+        {
+            // Slight downward velocity to keep grounded stable
+            if (playerVelocity.y < -2f)
+                playerVelocity.y = -2f;
+        }
+
+        // Read input
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        // Jump using WasPressedThisFrame()
+        if (groundedPlayer && jumpAction.action.WasPressedThisFrame())
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Move
+        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
+        controller.Move(finalMove * Time.deltaTime);
     }
 }

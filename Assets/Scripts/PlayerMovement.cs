a326860891f8a739bff503876b2642becc54405f
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-05 10:38:30 -0700

    Working Movement!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //TODO: FIX
-    //float Speed = 20;
-    //float Jump = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
 
-    // this is a update that happends every couple of frames rather than everyframe
     void Update()
     {
-        if (Input.GetAxis("vertical"))
-        {
-            Debug.Log("forward");
-        }
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
     }
 }

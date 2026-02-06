99c9d7095ab3e967e2d3bead44d0feffb9248cee
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-06 10:52:50 -0700

    Possibly working scripts?

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Player PlayerScript;
     public float PlayerDet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //          
         //the line below is having issues
-        //PlayerDet = Player.DetectSpeed;  
+        PlayerDet = PlayerScript.DetectSpeed;  
+        Debug.Log(PlayerDet);
     }
 
     // Update is called once per frame

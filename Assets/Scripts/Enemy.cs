fa8a32bc4afdfbc2f8768176d764f5195fdaa353
colten leany <coltenleany@gmail.com>
2026-02-07 12:14:48 -0700

    i think i fixed it but.... lets see?

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-using UnityEngine.SceneMangement;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     private Player PlayerScript;
     public float PlayerDet;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,19 +15,20 @@ public class Enemy : MonoBehaviour
         PlayerDet = PlayerScript.DetectSpeed;  
         Debug.Log(PlayerDet);
     }
+    public float hitRadiusDistance = 10f;
 
     // Update is called once per frame
     void LateUpdate()
     {
         var ray1 = new Ray( transform.position, transform.forward ); //casts a ray for player detection
                     
-        var hit : RaycastHit; 
+        RaycastHit hit;
                         
-        if( Physics.Raycast( ray1, hit, int_HitRadiusDistance ))
+        if( Physics.Raycast(ray1, out hit, hitRadiusDistance ))
         { 
             if( (hit.collider.name == "FPS") || (hit.collider.name == "Player") ) 
             { 	
-                SceneManger.LoadScene("GameOverScene");
+                SceneManager.LoadScene("GameOverScene");
                 //currently no scene named GameOverScene, but will test.
             } 
         } 

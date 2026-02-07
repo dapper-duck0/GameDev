69c338fb4831fc3a44d46dbd1695c923271fa4fd
colten leany <coltenleany@gmail.com>
2026-02-06 18:25:53 -0700

    i added a reference to player script for hight change

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
+    //move to player
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+
+    //for the steath mode
+    public Player stealth;
   
     void Update ()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
+
+        if (stealth == true)
+        {
+            //will move the camara down but for now nothing
+        }
     }
 }

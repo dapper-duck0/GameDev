e197eaeaffe0e54f2492d620c0ffd77a9ce7db9a
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-13 08:36:14 -0700

    almost working camera

--- Assets/Scripts/ViewingCamera.cs
+++ Assets/Scripts/ViewingCamera.cs
@@ -5,7 +5,7 @@ public class ViewingCamera : MonoBehaviour
     public float offset= 0.1f;
     public float normal = 0.8f;
     public bool IsCrouched = false;
-    
+
     void Start ()
     {
     
@@ -14,11 +14,11 @@ public class ViewingCamera : MonoBehaviour
   
     void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl) && IsCrouched = false)
+        if (Input.GetKeyDown(KeyCode.LeftControl) && IsCrouched == false)
         {
-            transform.Translate(0, normal-offset, 0);
+            transform.Translate(0, -(normal-offset), 0);
         }
-        else if (IsCrouched = true && Input.GetKeyUp(KeyCode.LeftControl))
+        else if (IsCrouched == true && Input.GetKeyUp(KeyCode.LeftControl))
         {
             transform.Translate(0, offset, 0);
         }

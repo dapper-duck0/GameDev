9c8f6a56576ff5d54479e337e017dc8ed34c95e2
colten leany <coltenleany@gmail.com>
2026-02-17 10:10:59 -0700

    updated player damidging

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour
 {
     private Player PlayerScript;
     public float PlayerDet;
+    public int DamidgePlayer = 5;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,14 +14,14 @@ public class Enemy : MonoBehaviour
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         PlayerScript = playerObj.GetComponent<Player>();
         //testing to see if PlayerDet is getting grabed 
-        PlayerDet = PlayerScript.DetectSpeed;  
-        Debug.Log("PlayerDet");
     }
     public float hitRadiusDistance = 10f;
 
     // Update is called once per frame
     void LateUpdate()
     {
-
+        PlayerDet = PlayerScript.DetectSpeed;  
+        PlayerScript.heath = heath - DamidgePlayer;
+        Debug(PlayerScript.heath)
     }
 }

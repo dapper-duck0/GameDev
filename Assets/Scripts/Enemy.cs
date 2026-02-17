d6091427f1831fe88483fe7362e535edc6080a7f
colten leany <coltenleany@gmail.com>
2026-02-17 10:02:37 -0700

    ahhh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ public class Enemy : MonoBehaviour
 {
     private Player PlayerScript;
     public float PlayerDet;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

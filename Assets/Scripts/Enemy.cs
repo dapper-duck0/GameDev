cde0f49747b77c6f3ca1f5942c33d903725e5736
D4ethW4tcher <khoenzesiger06@gmail.com>
2026-02-24 10:30:36 -0700

    code organization

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,11 @@ public class Enemy : MonoBehaviour
 {
     private Player PlayerScript;
     private ViewingCamera Camera;
-
     public float PlayerDet;
     public float hitRadiusDistance = 10f;
-
     public int DamidgePlayer = 5;
     public int timeRemaining;
     public int TimerAddTime;
-
     public bool DamidgingHappen = false;
     public bool Agro = false;
     public bool StopTimer = false;
@@ -27,10 +24,12 @@ public class Enemy : MonoBehaviour
             yield return new WaitForSeconds(1f);
             timeRemaining--;
             Debug.Log(timeRemaining);
+
             if (StopTimer == true)
             {
                 break;
             }
+
             if (TimerAddTime > 0){
                 timeRemaining += TimerAddTime;
             }
@@ -40,24 +39,26 @@ public class Enemy : MonoBehaviour
         Agro = true;
     }
 
-    void Start()
+    //to start a timer
+    public void StartTimer(int timeWanted)
     {
-        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
-        PlayerScript = playerObj.GetComponent<Player>();
-        //testing to see if PlayerDet is getting grabed 
+        timeRemaining = timeWanted;
+        StartCoroutine(Countdown());
     }
-    
+
     private void OnTriggerEnter(Collider other) // needs to add a collider box around enemy that is trigger.
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("player entered the enemy sights");
             transform.LookAt(other.transform);
+
             if (Camera.IsCrouched == true)
             {
                 Debug.Log("Time to find longer");
                 StartTimer(5);
             }
+
             else
             {
                 Debug.Log("time to find base speed");
@@ -67,18 +68,15 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        PlayerScript = playerObj.GetComponent<Player>();
+        //testing to see if PlayerDet is getting grabed 
+    }
 
     void LateUpdate()
     {
-
         PlayerDet = PlayerScript.DetectSpeed;
     }
-
-    //to start a timer
-    public void StartTimer(int timeWanted)
-    {
-        timeRemaining = timeWanted;
-        StartCoroutine(Countdown());
-    }
-
 }

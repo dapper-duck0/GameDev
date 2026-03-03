b27195d818b118d9f54f0f6b4ad67ee49d6f663f
colten leany <coltenleany@gmail.com>
2026-03-03 10:32:06 -0700

    this should move enemy to random location. i will probably make this better.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    void start
+    Random rnd = new Random();
+    void start()
     {
-
+        public Vector3 targetPosition = new Vector3(randomX, 0f, RandomZ); 
+        public float speed = 5f;
     }
-    void Update
+    void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * deltaTime);
+        if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
+        {
+            RandomPositionGen();
+            public Vector3 targetPosition = new Vector3(randomX, 0f, RandomZ); 
+        }
+    }
+    int RandomPositionGen()
+    {
+        int randomZ = rnd.Next(1, 26);
+        int randomX = rnd.Next(1, 26);
     }
 }

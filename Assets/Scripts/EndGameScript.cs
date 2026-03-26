447853268d725424c1cd3ba294c26dbf365f5fec
colten leany <coltenleany@gmail.com>
2026-03-26 10:16:08 -0600

    EndGameScript should be done

--- Assets/Scripts/EndGameScript.cs
+++ Assets/Scripts/EndGameScript.cs
@@ -4,10 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class EndGameScript : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private InputAction Interact;
+
+    void Start() {
+        Interact = playerInput.actions["Interact"];
+    }
+
     public void LoadNextSceneByName()
     {
+        private void OnTriggerEnter(Collider other)
+        {
+            // Check if the entering object has the "Player" tag
+            if (other.CompareTag("Player") && Interact.IsPressed())
+            {
+                SceneManager.LoadScene("EndScene");
+                //this should be a scene we need to create to play an end screan animation
+                
+            }
+        }
         
-        // Replace "SceneName" with the exact name of your target scene
-        SceneManager.LoadScene("EndScene");
+        
     }
 }
